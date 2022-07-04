018a0e48ea8d2e5299ee0bd68d9afecf65732328
José Vitor <josevitorpignataro@gmail.com>
2022-07-04 20:34:36 -0300

    Iniciando o projeto orçamento pessoal

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Class despesa
+class Despesa{
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+
+//Função que cria um despesa
+function cadastrarDespesa(){
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    let newDespesa = new Despesa(
+        ano.value, 
+        mes.value, 
+        dia.value, 
+        tipo.value, 
+        descricao.value, 
+        valor.value
+    )
+
+    console.log(newDespesa);
+}
+
+
